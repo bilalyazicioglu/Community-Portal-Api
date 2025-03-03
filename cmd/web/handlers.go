f8d28272e57efe0724007186838e0b2d61474aed
kavuni <ahmetbilalyazicioglu@gmail.com>
2025-03-03 16:55:17 +0300

    Yeni handler ve validator dosyaları eklendi

---
+++ cmd/web/handlers.go
@@ -0,0 +1,20 @@
+package web
+
+import (
+	"net/http"
+)
+
+// Create a new userSignupForm struct.
+type userSignupForm struct {
+	Name                string `form:"name"`
+	Email               string `form:"email"`
+	Password            string `form:"password"`
+	validator.Validator `form:"-"`
+}
+
+// Update the handler so it displays the signup page.
+func (app *application) userSignup(w http.ResponseWriter, r *http.Request) {
+	data := app.newTemplateData(r)
+	data.Form = userSignupForm{}
+	app.render(w, r, http.StatusOK, "signup.tmpl", data)
+}
