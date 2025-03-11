71453ad935638b8dd923d0e67264dd217ab79ad3
kavuni <ahmetbilalyazicioglu@gmail.com>
2025-03-11 23:59:49 +0300

    Get Firstname lastname and mail with tokens

--- internal/api/user.go
+++ internal/api/user.go
@@ -36,3 +36,30 @@ func (ro *Router) CreateUser(w http.ResponseWriter, r *http.Request) {
 
 	utils.JSONResponse(w, http.StatusCreated, result)
 }
+
+func GetFirstNameFromClaims(claims map[string]any) (string, bool) {
+	firstNameInterface, ok := claims["first_name"]
+	if !ok || firstNameInterface == nil {
+		return "", false
+	}
+	FirstName, ok := firstNameInterface.(string)
+	return FirstName, ok
+}
+
+func GetLastNameFromClaims(claims map[string]any) (string, bool) {
+	lastNameInterface, ok := claims["last_name"]
+	if !ok || lastNameInterface == nil {
+		return "", false
+	}
+	LastName, ok := lastNameInterface.(string)
+	return LastName, ok
+}
+
+func GetMailFromClaims(claims map[string]any) (string, bool) {
+	mailInterface, ok := claims["email"]
+	if !ok || mailInterface == nil {
+		return "", false
+	}
+	Email, ok := mailInterface.(string)
+	return Email, ok
+}
